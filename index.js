7d123c2ec39ef7e7f64dee8754fad224d1078b57
Vadim Kotov <igeeko@gmail.com>
2016-11-25 12:50:36 +0600

    basic session-controller functionality

---
+++ index.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStorage } from 'object-state-storage';
+
+class Session {
+  constructor(props) {
+    const {
+      mountPoint,
+      controllers,
+    } = props;
+
+    if (!mountPoint) {
+      throw new Error('Mount point is not defined');
+    }
+
+    if (Object.keys(controllers).length === 0) {
+      throw new Error('Provide controller_name : (cb) => require.ensure(...) map');
+    }
+
+    this._mountPoint = mountPoint;
+    this._controllers = controllers;
+    this._mounting = false;
+
+    this._store = createStorage({});
+
+    // bind!
+    this.setCurrentController = this.setCurrentController.bind(this);
+    this._render = this._render.bind(this);
+
+    // re-render controller's view on store updates
+    this._store.subscribe(() => {
+      if (this._unmounting) {
+        this._render();
+      }
+    });
+  }
+  setCurrentController(name, props) {
+    // get wrapper for require.ensure
+    const ensureController = this._controllers[name];
+
+    if (!ensureController) {
+      throw new Error(`Controller ${name} not found`);
+    }
+
+    // load controller
+    // NOTE: failed chunk requests are not handled
+    ensureController((Controller) => {
+      // dispose previous controller
+      this.controller.dispose();
+
+      // disable re-rendering
+      this._unmounting = true;
+
+      // unmount view
+      ReactDOM.unmountComponentAtNode(this._mountPoint);
+
+      // create a new controller
+      // pass Session's actions and context
+      this.controller = new Controller(props, this.context, this.actions);
+
+      // enable re-rendering
+      this._unmounting = false;
+
+      // render new view
+      this._render();
+    });
+  }
+  _render() {
+    ReactDOM.render(
+      React.createElement(this.controller.view),
+      this._mountPoint,
+    );
+  }
+  get actions() {
+    return {
+      setCurrentController: this.setCurrentController,
+    };
+  }
+  get context() {
+    return {
+      store: this._store,
+    };
+  }
+}
+
+export default Session;
